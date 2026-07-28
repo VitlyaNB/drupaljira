ebb7289192c8cbf3a55c13d8791512818f47e46f
VitalyaNB <vitalya.khomich15@gmail.com>
2026-07-29 00:34:59 +0300

    setup config sync directory and document workflow

---
+++ web/sites/default/files/php/twig/6a691aa192514_item-list.html.twig_XUHlDuWdAaKVQTpIBGKK2Ehuy/urcxsisHQ9ydGkE3wBCTnzzgllWDS0c_c44cCGPq0OA.php
@@ -0,0 +1,160 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\CoreExtension;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Sandbox\SecurityNotAllowedTestError;
+use Twig\Source;
+use Twig\Template;
+use Twig\TemplateWrapper;
+
+/* core/modules/system/templates/item-list.html.twig */
+class __TwigTemplate_21c6aabd3fb3ac45f5566f098cd319e5 extends Template
+{
+    private Source $source;
+    /**
+     * @var array<string, Template>
+     */
+    private array $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->extensions[SandboxExtension::class];
+    }
+
+    protected function doDisplay(array $context, array $blocks = []): iterable
+    {
+        $macros = $this->macros;
+        // line 24
+        if ((($tmp = CoreExtension::getAttribute($this->env, $this->source, ($context["context"] ?? null), "list_style", [], "any", false, false, true, 24)) && $tmp instanceof Markup ? (string) $tmp : $tmp)) {
+            // line 25
+            $context["attributes"] = CoreExtension::getAttribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [("item-list__" . CoreExtension::getAttribute($this->env, $this->source, ($context["context"] ?? null), "list_style", [], "any", false, false, true, 25))], "method", false, false, true, 25);
+        }
+        // line 27
+        if (((($tmp = ($context["items"] ?? null)) && $tmp instanceof Markup ? (string) $tmp : $tmp) || (($tmp = ($context["empty"] ?? null)) && $tmp instanceof Markup ? (string) $tmp : $tmp))) {
+            // line 28
+            if ( !Twig\Extension\CoreExtension::testEmpty(($context["title"] ?? null))) {
+                // line 29
+                yield "<h3>";
+                yield (string) $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, ($context["title"] ?? null), "html", null, true);
+                yield "</h3>";
+            }
+            // line 32
+            if ((($tmp = ($context["items"] ?? null)) && $tmp instanceof Markup ? (string) $tmp : $tmp)) {
+                // line 33
+                yield "<";
+                yield (string) $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, ($context["list_type"] ?? null), "html", null, true);
+                yield (string) $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, ($context["attributes"] ?? null), "html", null, true);
+                yield ">";
+                // line 34
+                $context['_parent'] = $context;
+                $context['_seq'] = CoreExtension::ensureTraversable(($context["items"] ?? null));
+                foreach ($context['_seq'] as $context["_key"] => $context["item"]) {
+                    // line 35
+                    yield "<li";
+                    yield (string) $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, CoreExtension::getAttribute($this->env, $this->source, $context["item"], "attributes", [], "any", false, false, true, 35), "html", null, true);
+                    yield ">";
+                    yield (string) $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, CoreExtension::getAttribute($this->env, $this->source, $context["item"], "value", [], "any", false, false, true, 35), "html", null, true);
+                    yield "</li>";
+                }
+                $_parent = $context['_parent'];
+                unset($context['_seq'], $context['_key'], $context['item'], $context['_parent']);
+                $context = array_intersect_key($context, $_parent);
+                $context += $_parent;
+                // line 37
+                yield "</";
+                yield (string) $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, ($context["list_type"] ?? null), "html", null, true);
+                yield ">";
+            } else {
+                // line 39
+                yield (string) $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, ($context["empty"] ?? null), "html", null, true);
+            }
+        }
+        $this->env->getExtension('\Drupal\Core\Template\TwigExtension')
+            ->checkDeprecations($context, ["context", "items", "empty", "title", "list_type"]);        yield from [];
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName(): string
+    {
+        return "core/modules/system/templates/item-list.html.twig";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function isTraitable(): bool
+    {
+        return false;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo(): array
+    {
+        return array (  85 => 39,  80 => 37,  69 => 35,  65 => 34,  60 => 33,  58 => 32,  53 => 29,  51 => 28,  49 => 27,  46 => 25,  44 => 24,);
+    }
+
+    public function getSourceContext(): Source
+    {
+        return new Source("", "core/modules/system/templates/item-list.html.twig", "/var/www/html/web/core/modules/system/templates/item-list.html.twig");
+    }
+    
+    public function ensureSecurityChecked(): void
+    {
+        if ($this->sandbox->isSandboxed($this->source)) {
+            $this->checkSecurity();
+        }
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = ["if" => 24, "set" => 25, "for" => 34];
+        static $filters = ["escape" => 29];
+        static $functions = [];
+        static $tests = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                [0 => "if", 1 => "set", 2 => "for"],
+                [0 => "escape"],
+                [],
+                [],
+                $this->source
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            } elseif ($e instanceof SecurityNotAllowedTestError && isset($tests[$e->getTestName()])) {
+                $e->setTemplateLine($tests[$e->getTestName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
